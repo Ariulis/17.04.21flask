faa48a0da990bfa52b08cc4758225f4cc27e5f66
Mothis <andrey59995@gmail.com>
2021-04-20 10:40:38 +0300

    created profile user, edit profile user functional, 404, 403, 500 errors handler

---
+++ app/main/forms.py
@@ -0,0 +1,41 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, TextAreaField, SelectField, SubmitField, ValidationError
+from wtforms.validators import DataRequired, Length, Email, Regexp
+
+from ..models import User, Role
+
+
+class EditAdminProfileForm(FlaskForm):
+    username = StringField('Username', validators=[
+        DataRequired(), Length(1, 64), Regexp(
+            r"^[a-zA-Z]+[a-zA-Z0-9._]*$", 0, 'Username must have only letters, figures, dotts or underscores')
+    ])
+    email = StringField('Email', validators=[
+        DataRequired(), Length(1, 64), Email()
+    ])
+    confirmed = BooleanField('Confirmed')
+    role = SelectField('Role', coerce=int)
+    name = StringField('Real name', validators=[Length(0, 32)])
+    location = StringField('Location', validators=[Length(0, 32)])
+    about_me = TextAreaField('About me')
+    submit = SubmitField('Change profile')
+
+    def __init__(self, user, *args, **kwargs):
+        super(EditAdminProfileForm, self).__init__(*args, **kwargs)
+        self.role.choices = [(role.id, role.name) for role in Role.query.all()]
+        self.user = user
+
+    def validate_username(self, field):
+        if field.data != self.user.username and User.query.filter_by(username=field.data).first():
+            raise ValidationError('Username is already in use')
+
+    def validate_email(self, field):
+        if field.data != self.user.email and User.query.filter_by(email=field.data).first():
+            raise ValidationError('Email is already registered')
+
+
+class EditProfileForm(FlaskForm):
+    name = StringField('Real name', validators=[Length(0, 32)])
+    location = StringField('Location', validators=[Length(0, 32)])
+    about_me = TextAreaField('About me')
+    submit = SubmitField('Change profile')
--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,66 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for, request
+from flask_login import login_required, current_user
 
 from . import main
+from .forms import EditProfileForm, EditAdminProfileForm
+from .. import db
+from ..models import User, Role
+from ..decorators import admin_required
 
 
 @main.route('/')
 def index():
     return render_template('index.html')
+
+
+@main.route('/profile/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    return render_template('user.html', user=user)
+
+
+@main.route('/edit-profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.name = form.name.data
+        current_user.location = form.location.data
+        current_user.about_me = form.about_me.data
+        db.session.add(current_user)
+        db.session.commit()
+        flash('The info has been changed.', 'info')
+        return redirect(url_for('main.user', username=current_user.username))
+    form.name.data = current_user.name
+    form.location.data = current_user.location
+    form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', form=form, username=current_user.username)
+
+
+@main.route('/edit-profile/<int:id>', methods=['GET', 'POST'])
+@login_required
+@admin_required
+def edit_admin_profile(id):
+    user = User.query.get_or_404(id)
+    form = EditAdminProfileForm(user=user)
+    if form.validate_on_submit():
+        user.username = form.username.data
+        user.email = form.email.data
+        user.confirmed = form.confirmed.data
+        user.role = Role.query.get(form.role.data)
+        user.name = form.name.data
+        user.location = form.location.data
+        user.about_me = form.about_me.data
+        db.session.add(current_user)
+        db.session.commit()
+        flash('The info has been changed.', 'info')
+        return redirect(url_for('main.user', username=user.username))
+    form.username.data = user.username
+    form.email.data = user.email
+    form.confirmed.data = user.confirmed
+    form.role.data = user.role_id
+    form.name.data = user.name
+    form.location.data = user.location
+    form.about_me.data = user.about_me
+    return render_template('edit_profile.html', form=form, username=user.username)
